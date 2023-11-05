be72fec282861d953095dee77b08b82f7d2ac093
Arsen290 <arsen290@ukr.net>
2023-11-05 19:11:20 +0100

    Make temporary UserService

--- src/main/java/cz/project/myfit/MyFitApplication.java
+++ src/main/java/cz/project/myfit/MyFitApplication.java
@@ -1,7 +1,11 @@
 package cz.project.myfit;
 
+import cz.project.myfit.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.List;
 
 @SpringBootApplication
 public class MyFitApplication {
@@ -9,5 +13,4 @@ public class MyFitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyFitApplication.class, args);
 	}
-
 }
---
+++ src/main/java/cz/project/myfit/services/UserService.java
@@ -0,0 +1,16 @@
+package cz.project.myfit.services;
+
+import cz.project.myfit.models.User;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserService {
+    public List<User> getUsers() {
+
+        return List.of(
+                new User(1, "Bob", "bob@mail.com")
+        );
+    }
+}
