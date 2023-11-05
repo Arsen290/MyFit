9559ba6bb6077d50c90d2221baa8097912a69b1d
Arsen290 <arsen290@ukr.net>
2023-11-05 14:36:32 +0100

    Initialising the Application

---
+++ src/test/java/cz/project/myfit/MyFitApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.project.myfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
