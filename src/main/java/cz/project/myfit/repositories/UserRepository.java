629af542e81a2fcdae77210b48913b8321525f66
Arsen290 <arsen290@ukr.net>
2023-11-05 21:07:12 +0100

    Create repository for User

---
+++ src/main/java/cz/project/myfit/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package cz.project.myfit.repositories;
+
+import cz.project.myfit.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
