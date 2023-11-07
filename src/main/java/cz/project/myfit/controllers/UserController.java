4fe2d80ece19b71d8f79c5445be929e234288256
Arsen290 <arsen290@ukr.net>
2023-11-07 16:57:57 +0100

    Delete UserService and create UserDAO

--- src/main/java/cz/project/myfit/controllers/UserController.java
+++ src/main/java/cz/project/myfit/controllers/UserController.java
@@ -1,9 +1,8 @@
 package cz.project.myfit.controllers;
 
+import cz.project.myfit.dao.UserDAO;
 import cz.project.myfit.models.User;
-import cz.project.myfit.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,33 +19,33 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    private final UserService userService;
+    private final UserDAO userDAO;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
     }
 
     @GetMapping
     public List<User> getUsers(){
-        return userService.getUsers();
+        return userDAO.getUsers();
     }
 
     @PostMapping
     public void addUser(@RequestBody User user){
-        userService.save(user);
+        userDAO.save(user);
     }
 
     @DeleteMapping(path = "{userId}")
     public void deleteUser(@PathVariable("userId") Long userId){
-        userService.deleteUser(userId);
+        userDAO.deleteUser(userId);
     }
 
     @PutMapping
     public void updateUser(@PathVariable("userId") Long userId,
                            @RequestParam(required = false) String name,
                            @RequestParam(required = false) String email){
-        userService.updateUser(userId, name , email);
+        userDAO.updateUser(userId, name , email);
     }
 
 }
