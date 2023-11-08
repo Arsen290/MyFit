e68db2d9ab34cf486fc1667c80749cfea5df1e52
Arsen290 <arsen290@ukr.net>
2023-11-08 20:13:43 +0100

    Create index and add dependencie <artifactId>spring-boot-starter-thymeleaf</artifactId> in pom

--- src/main/java/cz/project/myfit/controllers/UserController.java
+++ src/main/java/cz/project/myfit/controllers/UserController.java
@@ -3,6 +3,7 @@ package cz.project.myfit.controllers;
 import cz.project.myfit.dao.UserDAO;
 import cz.project.myfit.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 public class UserController {
-
     private final UserDAO userDAO;
 
     @Autowired
@@ -27,28 +28,28 @@ public class UserController {
         this.userDAO = userDAO;
     }
 
-    @GetMapping
+    @GetMapping("")
     public String getUsers(Model model) {
         List<User> users = userDAO.getUsers();
         model.addAttribute("users", users);
-        return "users/show";
+        return "users/index";
     }
-
-    @PostMapping
+    @ResponseBody
+    @PostMapping(value = "post")
     public void addUser(@RequestBody User user){
         userDAO.save(user);
     }
-
-    @DeleteMapping(path = "{userId}")
-    public void deleteUser(@PathVariable("userId") Long userId){
-        userDAO.deleteUser(userId);
-    }
-
-    @PutMapping
-    public void updateUser(@PathVariable("userId") Long userId,
-                           @RequestParam(required = false) String name,
-                           @RequestParam(required = false) String email){
-        userDAO.updateUser(userId, name , email);
-    }
+//
+//    @DeleteMapping(path = "{userId}")
+//    public void deleteUser(@PathVariable("userId") Long userId){
+//        userDAO.deleteUser(userId);
+//    }
+//
+//    @PutMapping
+//    public void updateUser(@PathVariable("userId") Long userId,
+//                           @RequestParam(required = false) String name,
+//                           @RequestParam(required = false) String email){
+//        userDAO.updateUser(userId, name , email);
+//    }
 
 }
