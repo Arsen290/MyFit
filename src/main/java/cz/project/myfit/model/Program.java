0c3d15e901ab7a37a1859dc9f1bc591db27b8de4
Arsen290 <arsen290@ukr.net>
2023-11-18 13:16:58 +0100

    Create model Program

---
+++ src/main/java/cz/project/myfit/model/Program.java
@@ -0,0 +1,71 @@
+package cz.project.myfit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "programs")
+public class Program {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "Program name should not be empty")
+    @Size(min = 2, max = 30, message = "Program name should be between 2 and 30 characters")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private LocalDate date;
+    public Program() {
+    }
+
+    public Program(String name, User user, LocalDate date) {
+        this.name = name;
+        this.user = user;
+        this.date = date;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+}
