f82fc76954b25431b29ffe3a348ed8296ed8ef73
Arsen290 <arsen290@ukr.net>
2023-11-22 20:24:00 +0100

    reating a pring ecurity ser object for authentication.

--- src/main/java/cz/project/myfit/service/UserService.java
+++ src/main/java/cz/project/myfit/service/UserService.java
@@ -1,15 +1,23 @@
 package cz.project.myfit.service;
 
 import cz.project.myfit.DTO.UserDTO;
+import cz.project.myfit.enums.UserRole;
+import cz.project.myfit.model.Role;
 import cz.project.myfit.model.User;
 import cz.project.myfit.repository.RoleRepository;
 import cz.project.myfit.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -56,5 +64,40 @@ public class UserService implements UserDetailsService {
 
     public void delete(Long id) {
         userRepository.deleteById(id);
+    }
+
+    // Creating a Spring Security User object for authentication.
+    // Setting the username (email), password, and authorities (roles) for the authenticated user.
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                // Mapping user roles to GrantedAuthority and collecting the mapped authorities into a list
+                user.getRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName().toString()))
+                        .collect(Collectors.toList()));
+    }
+
+    public void createNewUser(UserDTO userDTO) {
+        try {
+        // Find the 'ROLE_USER' role using the roleService
+        Role userRole = roleService.findRoleByName(UserRole.USER);
+
+        // Set the user's roles to a Set containing only the 'ROLE_USER' role
+        userDTO.setRoles(Set.of(userRole));
+
+        // Create a new User object from the UserDTO
+        User user = new User(userDTO.getId(), userDTO.getName(), userDTO.getPassword(), userDTO.getEmail(), userDTO.getRoles());
+
+        // Save the user to the userRepository
+        userRepository.save(user);}
+        catch (DataIntegrityViolationException e) {
+            // Handle the exception (e.g., log an error or throw a custom exception)
+            throw new IllegalStateException("Duplicate name or email");
+        }
     }
 }
